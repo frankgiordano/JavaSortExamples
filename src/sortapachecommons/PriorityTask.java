f58dd4a13d7d3edfa66892673e132d10d5f6016c
francesco.giordano <francesco.giordano@coxautoinc.com>
2018-09-16 15:59:11 -0400

    added apache commons example

---
+++ src/sortapachecommons/PriorityTask.java
@@ -0,0 +1,42 @@
+package sortapachecommons;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PriorityTask {
+		
+	private int id;
+	private boolean mustGo;
+	private LocalDate deadLineDate;
+	private LocalDateTime startDateTime;
+		
+	public PriorityTask(int id, boolean mustGo, LocalDate deadLineDate, LocalDateTime startDateTime) {
+		super();
+		this.id = id;
+		this.mustGo = mustGo;
+		this.deadLineDate = deadLineDate;
+		this.startDateTime = startDateTime;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public boolean isMustGo() {
+		return mustGo;
+	}
+
+	public LocalDate getDeadLineDate() {
+		return deadLineDate;
+	}
+
+	public LocalDateTime getStartDateTime() {
+		return startDateTime;
+	}
+
+	@Override
+	public String toString() {
+		return "PriorityTask [id=" + id + ", mustGo=" + mustGo + ", deadLineDate=" + deadLineDate + ", startDateTime="
+				+ startDateTime + "]";
+	}
+		
+}
